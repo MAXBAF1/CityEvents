de9c88e3b2269fba519ea71623898be497508d28
lloppy <ankudinovapol@gmail.com>
2023-08-13 02:28:32 +0500

    add icons and readme

---
+++ app/src/main/java/com/example/cityevents/adapters/ViewPagerEventAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.cityevents.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cityevents.R
+import com.squareup.picasso.Picasso
+
+class ViewPagerEventAdapter(private val imageList: List<String>) :
+    RecyclerView.Adapter<ViewPagerEventAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageUrl = imageList[position]
+        Picasso.get().load(imageUrl).into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int = imageList.size
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+}
